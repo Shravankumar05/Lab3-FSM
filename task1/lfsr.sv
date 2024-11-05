9c572f9f026024d0d5c20f66ab4859007edd3d1a
Shravankumar05 <mshravan005@gmail.com>
2024-11-05 17:45:22 +0000

    task 1 completed

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -5,4 +5,12 @@ module lfsr(
     output  logic [3:0] data_out
 );
 
+logic [3:0] sreg;
+
+always_ff @(posedge clk) begin
+    if(rst) sreg <= 4'b1;
+    else sreg <= {sreg[2:0], sreg[3]^sreg[2]};
+end
+
+assign data_out = sreg;
 endmodule
